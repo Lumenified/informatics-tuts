232f91d9566f359962b4dfb9e8e5264dd7691197
Lumenified <lumenified@gmail.com>
2019-05-24 12:30:56 +0300

    networkxtutsfin -fixes

--- tutorials/tuts#2.py
+++ tutorials/tuts#2.py
@@ -51,7 +51,7 @@ for i,nlrow in nodelist.iterrows():
 ################################
 
 ### Define node positions data structure (dict) for plotting
-node_positions = {node[0]: (node[1]['X'], -node[1]['Y']) for node in g.nodes(data=True)}
+NODE_POSITIONS = {node[0]: (node[1]['X'], -node[1]['Y']) for node in g.nodes(data=True)}
 
 #print(node_positions)#previews the positions
 #print(dict(list(node_positions.items())[0:5]))#previews the first 5 items of positions in position list
@@ -62,12 +62,12 @@ node_positions = {node[0]: (node[1]['X'], -node[1]['Y']) for node in g.nodes(dat
 #print(list(g.edges(data=True))[2][2])
 my_list = list(g.edges(data=True))[0][2]
 #print(dict(my_list)['attr_dict']['color'])
-edge_colors = [dict(attr[2])['attr_dict']['color'] for attr in list(g.edges(data=True))]
+EDGE_COLORS = [dict(attr[2])['attr_dict']['color'] for attr in list(g.edges(data=True))]
 ###PROBLEMATIC###
 #edge_colors = [e[2]['color'] for e in list(g.edges(data=True))]
 ###PROBLEMATIC###
 #print(edge_colors[0:10])
 plt.figure(figsize=(8, 6))
-nx.draw(g, pos=node_positions, edge_color=edge_colors, node_size=10, node_color='black')
+nx.draw(g, pos=NODE_POSITIONS, edge_color=EDGE_COLORS, node_size=10, node_color='black')
 plt.title('raph Representation of Sleeping Giant Trail Map', size=15)
 plt.show()
