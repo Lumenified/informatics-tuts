97b25d439b5bff426300eb36da39b928669fc98f
Lumenified <lumenified@gmail.com>
2019-06-17 19:59:12 +0300

    two typed mendel's first law

--- tutorials/rosalindProblems/tuts7-2.py
+++ tutorials/rosalindProblems/tuts7-2.py
@@ -1,7 +1,6 @@
 with open("tuts7.txt") as f:
     my_file = [int(i) for i in str(f.read()).split()]
 
-
 def second_edit(pop: dict):
     t2,t3,p = pop[1],pop[2],pop[0]+pop[1]+pop[2]
     return 1 - (( t2*t3 + .25*t2*(t2-1) + t3*(t3-1) ) / ( p*(p-1) ))
